57f409b662bd0f83d985cd1bfdab72ff6d6054c4
Enuel <enuel.monzon@gmail.com>
2022-08-06 05:10:38 -0300

    added auth components

--- front-alcance/src/main.jsx
+++ front-alcance/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { AlcanceApp } from './AlcanceApp';
-// import './index.css'
+import { MainPageAuth } from './auth';
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
