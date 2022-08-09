23c74d48ac78b824828f137b44256598cf663818
Enuel <enuel.monzon@gmail.com>
2022-08-09 14:39:16 -0300

    creating login & signup components

--- front-alcance/src/auth/components/SignupForm.jsx
+++ front-alcance/src/auth/components/SignupForm.jsx
@@ -1,7 +1,28 @@
+import { useContext } from "react";
+import { BoxContainer, FormContainer, Input, MutedLink, SubmitButton, Marginer, BoldLink, AccountContext } from "../"
+
+
 
 
 export const SignupForm = () => {
+
+  const { switchToSignin } = useContext(AccountContext);
+
   return (
-    <div>SignupForm</div>
+    <BoxContainer>
+      <FormContainer>
+        <Input type="email" placeholder="Correo electrónico" />
+        <Input type="password" placeholder="Contraseña" />
+        <Input type="password" placeholder="Confirmar contraseña" />
+        </FormContainer>
+        <Marginer direction="vertical" margin={10} />
+        <MutedLink href="#">¿Olvidó su contraseña?</MutedLink>
+        <Marginer direction="vertical" margin="1.6em" />
+        <SubmitButton type="submit" > Entrar </SubmitButton>
+        <Marginer direction="vertical" margin="1em" />
+        <MutedLink>
+        ¿Ya tiene una cuenta? <BoldLink onClick={ switchToSignin } >Ingresar</BoldLink>
+        </MutedLink>
+    </BoxContainer>
   )
 }
--- front-alcance/src/auth/styled/common.jsx
+++ front-alcance/src/auth/styled/common.jsx
@@ -24,7 +24,7 @@ export const MutedLink = styled.a`
 
 export const BoldLink = styled.a`
   font-size: 11px;
-  color: rgb(241, 196, 15);
+  color: rgb(0,217,174);
   font-weight: 500;
   text-decoration: none;
   margin: 0 4px;
@@ -47,7 +47,7 @@ export const Input = styled.input`
   }
   &:focus {
     outline: none;
-    border-bottom: 2px solid rgb(241, 196, 15);
+    border-bottom: 2px solid rgb(0,217,174);
   }
 `;
 
@@ -61,11 +61,11 @@ export const SubmitButton = styled.button`
   border-radius: 100px 100px 100px 100px;
   cursor: pointer;
   transition: all, 240ms ease-in-out;
-  background: rgb(241, 196, 15);
+  background: rgb(0,217,174);
   background: linear-gradient(
     58deg,
-    rgba(241, 196, 15, 1) 20%,
-    rgba(243, 172, 18, 1) 100%
+    rgba(0,217,174,1) 20%,
+    rgba(26,188,156,1) 100%
   );
   &:hover {
     filter: brightness(1.03);
