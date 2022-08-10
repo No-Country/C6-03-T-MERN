42ec646b2a52dd6e06251e4af31d3754845f4b7a
Enuel <enuel.monzon@gmail.com>
2022-08-10 03:56:45 -0300

    creating router component

--- front-alcance/src/main.jsx
+++ front-alcance/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { AlcanceApp } from './AlcanceApp';
-import { MainPageAuth } from './auth';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MainPageAuth />
+    <AlcanceApp />
   </React.StrictMode>
 )
