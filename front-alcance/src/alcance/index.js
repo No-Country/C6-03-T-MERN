35d7e34ea3bb71aca7d5b49c17e08ad3a46cb893
diegonicita <diegonicita@gmail.com>
2022-08-24 13:18:54 -0300

    Fix Chat path in index.js

--- front-alcance/src/alcance/index.js
+++ front-alcance/src/alcance/index.js
@@ -2,7 +2,7 @@
 
 export * from './components/CalendarEventBox';
 export * from './components/CalendarModal';
-export * from './components/Chat';
+export * from './components/Chat/Chat';
 export * from './components/FabAddNew';
 export * from './components/FabDelete';
 export * from './components/Navbar';
