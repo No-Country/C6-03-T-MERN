99e8a065ce794ac62c203d4c6f34a4ea757af625
Enuel <enuel.monzon@gmail.com>
2022-08-24 00:18:01 -0300

    auth connection

--- front-alcance/src/router/AppRouter.jsx
+++ front-alcance/src/router/AppRouter.jsx
@@ -1,17 +1,29 @@
+import { useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { AlcancePage } from "../alcance";
 import { MainPageAuth } from "../auth";
+import { useAuthStore } from "../hooks";
 
 
 export const AppRouter = () => {
 
-  // Temporal ToDo: Token
-  const authStatus = 'authenticated'; //'authenticated'
+  const { status, checkAuthToken } = useAuthStore();
+  //const authStatus = 'not-authenticated'; //'authenticated'
+
+  useEffect(() => {
+    checkAuthToken();
+  }, []);
+
+  if ( status === 'checking' ) {
+    return (
+      <h3>Cargando...</h3>
+    )
+  }
 
   return (
     <Routes>
       {
-        ( authStatus === "not-authenticated" )
+        ( status === "not-authenticated" )
           ? (
             <>
               <Route path="/auth/*" element={<MainPageAuth />} />
