b2b95b02b021e7b75ed00053d7fca65cc4639dfb
diegonicita <diegonicita@gmail.com>
2022-08-25 23:53:51 -0300

    revert app router changes

--- front-alcance/src/router/AppRouter.jsx
+++ front-alcance/src/router/AppRouter.jsx
@@ -1,34 +1,31 @@
-import { useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { AlcancePage } from "../alcance";
 import { MainPageAuth } from "../auth";
-import { useAuthStore } from "../hooks";
 
 
 export const AppRouter = () => {
 
-  const { status, checkAuthToken } = useAuthStore();
-  //const authStatus = 'not-authenticated'; //'authenticated'
-
-  useEffect(() => {
-    checkAuthToken();
-  }, []);
-
-  if ( status === 'checking' ) {
-    return (
-      <h3>Cargando...</h3>
-    )
-  }
+  // Temporal ToDo: Token
+  const authStatus = 'not-authenticated'; //'authenticated'
 
   return (
     <Routes>
-      
-       
+      {
+        ( authStatus === "authenticated" )
+          ? (
+            <>
+              <Route path="/auth/*" element={<MainPageAuth />} />
+              <Route path="/*" element={<Navigate to="/auth/login" />} />
+            </>
+          )git 
+          : (
+            <>
               <Route path="/" element={<AlcancePage />} >
               </Route>
               <Route path="/*" element={<Navigate to="/" />} />
-          
-      
+            </>
+          )
+      }
     </Routes>
   )
 }
