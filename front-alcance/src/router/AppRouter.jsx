63f4a29a5c231df059ee5f13584eb7c14a8ffc49
diegonicita <diegonicita@gmail.com>
2022-08-25 23:58:25 -0300

    fix router

--- front-alcance/src/router/AppRouter.jsx
+++ front-alcance/src/router/AppRouter.jsx
@@ -1,23 +1,35 @@
+import { useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { AlcancePage } from "../alcance";
 import { MainPageAuth } from "../auth";
+import { useAuthStore } from "../hooks";
 
 
 export const AppRouter = () => {
 
-  // Temporal ToDo: Token
-  const authStatus = 'not-authenticated'; //'authenticated'
+  const { status, checkAuthToken } = useAuthStore();
+  //const authStatus = 'not-authenticated'; //'authenticated'
+
+  useEffect(() => {
+    checkAuthToken();
+  }, []);
+
+  if ( status === 'checking' ) {
+    return (
+      <h3>Cargando...</h3>
+    )
+  }
 
   return (
     <Routes>
       {
-        ( authStatus === "authenticated" )
+        ( status === "not-authenticated" )
           ? (
             <>
               <Route path="/auth/*" element={<MainPageAuth />} />
               <Route path="/*" element={<Navigate to="/auth/login" />} />
             </>
-          )git 
+          )
           : (
             <>
               <Route path="/" element={<AlcancePage />} >
