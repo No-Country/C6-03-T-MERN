97fcec5633070e9eb02848fb35893916c869400c
diegonicita <diegonicita@gmail.com>
2022-09-02 09:15:41 -0300

    Add username to PreHome

--- front-alcance/src/alcance/components/waitingRoom/src/components/Users.jsx
+++ front-alcance/src/alcance/components/waitingRoom/src/components/Users.jsx
@@ -2,11 +2,17 @@
 import { FaUserCircle } from "react-icons/fa";
 import "./styles/usersStyle.css";
 
+import { useSelector} from "react-redux"
+
 export default function Users({ infoUser }) {
+
+  const { status, user : username, errorMessage } = useSelector( state => state.auth );
+  console.log("user: " + username.name)
+
   return (
     <div className="userCard">
       <FaUserCircle />
-      <h5>Bienvenido "usuario1"!</h5>
+      <h5>Bienvenido {username.name}!</h5>
     </div>
   );
 }
