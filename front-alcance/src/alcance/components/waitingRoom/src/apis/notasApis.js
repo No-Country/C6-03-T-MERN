37bd3b570da041177a075246a87dd269cc370020
diegonicita <diegonicita@gmail.com>
2022-09-02 09:36:17 -0300

    Call API to Get List of Users

--- front-alcance/src/alcance/components/waitingRoom/src/apis/notasApis.js
+++ front-alcance/src/alcance/components/waitingRoom/src/apis/notasApis.js
@@ -2,5 +2,8 @@ import axios from 'axios'
 import { getEnvVariables } from '../../../../../helpers'
 const { VITE_API_URL } = getEnvVariables()
 
-export const getUsers = async () =>  
-    await axios.get(VITE_API_URL + "/users/list/")
+export const getUsers = async () =>
+  await axios.get(VITE_API_URL + '/users/list/')
+
+export const getProjects = async () =>
+  await axios.get(VITE_API_URL + '/projects/list/')
--- front-alcance/src/alcance/components/waitingRoom/src/components/ContainerTeams.jsx
+++ front-alcance/src/alcance/components/waitingRoom/src/components/ContainerTeams.jsx
@@ -1,23 +1,32 @@
 import ModalTeam from "./ModalTeam";
 import React from "react";
-import {useEffect} from "react"
-import {getUsers} from "../apis/notasApis"
+import {useEffect, useState} from "react"
+import {getUsers, getProjects} from "../apis/notasApis"
 import "./styles/usersStyle.css";
+import { getEnvVariables } from '../../../../../helpers'
+const { VITE_API_URL } = getEnvVariables()
 
 export default function ContainerTeams({ infoUser }) {
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [projectList, setProjectList] = useState([])
   
   useEffect(() => {
-    async function projectsUser(){
-      const response = await getUsers()
-      console.log(response.data)
+    async function callApi(){
+      const response = await getProjects()      
+      setProjectList(response.data.data)
+      setIsLoading(false)
     }
-    projectsUser()
+    callApi()
   }, [])
   
   return (
     <div className="container-teams">
       <h5>Tus equipos de trabajo</h5>
-      <ModalTeam projectsId={["12335321", "12335321", "12335321"]} />
+      {!isLoading && Array.isArray(projectList) && projectList > 0 &&
+      <ModalTeam projectsId={["12335321", "12335321", "12335321"]} />}
+      {!isLoading && (!Array.isArray(projectList) || projectList.length === 0) && 
+      <div style={{textAlign: "center", margin: "1rem"}}> No tienes equipos asignados </div>}    
     </div>
   );
 }
